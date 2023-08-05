1e1731123aef56ef904b11023673019d6ec81ca4
Jenifer Nguyen <jwinn19464@gmail.com>
2023-08-04 20:12:23 -0400

    added main function

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 # name = input("What's your name? ").strip().title()
 
 # print(f"hello, {name}")
-def hello(name):
+def main():
+    name = input("What's your name? ")
+    hello(name)
+
+
+def hello(name="world"):
     print("Hello", name)
 
-name = input("What's your name? ")
-hello(name)
+main()
