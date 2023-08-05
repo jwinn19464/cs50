4cd3d3926bd10bda4a78b030a09d0ad58d4b8191
Jenifer Nguyen <jwinn19464@gmail.com>
2023-08-04 20:19:33 -0400

    Used the pow function

--- calculator.py
+++ calculator.py
@@ -6,10 +6,10 @@
 # print(z)
 
 def main():
-    x = int(input("What's x?"))
+    x = int(input("What's x? "))
     print("x squared is", square(x))
 
 def square(n):
-    return n * n
+    return pow(n, 2)
 
 main()
