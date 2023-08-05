afc7e7808990f4f521bdfa7a1078ff7156a96a38
Jenifer Nguyen <jwinn19464@gmail.com>
2023-08-04 20:03:11 -0400

    hello function with default value added

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-name = input("What's your name? ").strip().title()
+# name = input("What's your name? ").strip().title()
 
-print(f"hello, {name}")
+# print(f"hello, {name}")
+def hello(name):
+    print("Hello", name)
+
+name = input("What's your name? ")
+hello(name)
