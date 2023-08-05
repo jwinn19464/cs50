92c8e23740bef709b4078dd3ba47675a80f2b629
Jenifer Nguyen <jwinn19464@gmail.com>
2023-08-04 20:18:14 -0400

    Added a square function

--- calculator.py
+++ calculator.py
@@ -1,5 +1,15 @@
-x = float(input("Give me a number "))
-y = float(input("Give me another number "))
+# x = float(input("Give me a number "))
+# y = float(input("Give me another number "))
 
-z = round(x + y)
-print(f"{z:,}")
+# z = round(x / y, 3)
+# # print(f"{z:,}")
+# print(z)
+
+def main():
+    x = int(input("What's x?"))
+    print("x squared is", square(x))
+
+def square(n):
+    return n * n
+
+main()
